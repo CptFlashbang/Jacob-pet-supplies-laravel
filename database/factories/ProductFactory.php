efd5ced73bb66d5e57744222aff35afd4ce10121
CptFlashbang <Callum.yates.180@gmail.com>
2025-02-06 17:15:36 +0000

    Enhance ProductFactory to include type and description attributes

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,8 +16,14 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $types = ['Toy', 'Grooming', 'Treat'];
+
         return [
-            //
+            'name' => $this->faker->words(3, true), // Generates a random product name
+            'price' => $this->faker->randomFloat(2, 0.5, 50), // Price between 0.50 and 50.00
+            'available' => $this->faker->boolean(80), // 80% chance of being true (available)
+            'description' => $this->faker->sentence(12), // Generates a random description
+            'type' => $this->faker->randomElement($types), // Randomly selects from the predefined types
         ];
     }
 }
