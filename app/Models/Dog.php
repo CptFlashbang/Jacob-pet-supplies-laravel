875aac7676865bda4d8d9102d1f3b013a09fc389
CptFlashbang <Callum.yates.180@gmail.com>
2025-02-06 17:36:35 +0000

    Add fillable properties to Dog model

--- app/Models/Dog.php
+++ app/Models/Dog.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Dog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'sex',
+        'breed',
+        'image',
+    ];
 }
