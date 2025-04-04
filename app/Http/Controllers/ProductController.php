d5722de4cf9d8fd707c97cc4e29e51cea18dfc0f
CptFlashbang <Callum.yates.180@gmail.com>
2025-04-04 14:41:47 +0100

    Group products by type in index method of ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $products = Product::all()->groupBy('type');
         return view('products.index', compact('products'));
     }
 
