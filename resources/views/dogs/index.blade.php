38534688d9d8013feb074cba3ec7a17a0c0e83ea
CptFlashbang <Callum.yates.180@gmail.com>
2025-04-07 16:56:08 +0100

    Refactor to use blade layout

--- resources/views/dogs/index.blade.php
+++ resources/views/dogs/index.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.guest')
-
-@section('content')
+<x-guest-layout>
     <div class="adoption-page">
         <header class="mb-6">
             <h1 class="text-3xl font-bold text-center">Adopt a Friend for Life</h1>
@@ -9,27 +7,19 @@
             </p>
         </header>
 
-        <div class="flex justify-center items-center">
-            <ul class="grid content-center justify-center grid-cols-2 gap-4 md:grid-cols-5">
-                @foreach ($dogs as $dog)
-                    <x-dog-card :dog="$dog" />
-                @endforeach
-            </ul>
-        </div>
+        <x-dog.dog-list :dogs="$dogs" />
 
         <section class="mt-8 text-center">
-            <a href="{{ route('dogs.index') }}">
-                <button class="bg-green-500 text-raisin-black-500 py-2 px-4 rounded hover:bg-green-600">
-                    View All Dogs Available for Adoption
-                </button>
-            </a>
+            <button class="bg-green-500 text-raisin-black-500 py-2 px-4 rounded hover:bg-green-600">
+                View All Dogs Available for Adoption
+            </button>
         </section>
 
         <section class="mt-12 text-center">
             <h2 class="text-2xl font-bold">Support Sherlock Rescue</h2>
             <p class="text-gray-700 mt-4">
-                Can’t adopt right now? Support Sherlock Rescue by donating or shopping at Jacob Pet Supplies, where a
-                percentage of certain sales goes directly to the rescue center.
+                Can’t adopt right now? Support Sherlock Rescue by donating or shopping at Jacob Pet Supplies,
+                where a percentage of certain sales goes directly to the rescue center.
             </p>
         </section>
 
@@ -42,4 +32,4 @@
             </p>
         </footer>
     </div>
-@endsection
+</x-guest-layout>
