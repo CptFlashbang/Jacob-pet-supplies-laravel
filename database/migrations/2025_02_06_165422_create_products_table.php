ed5791d6d5d542e4c3a8fe606529d98d83ac0cc5
CptFlashbang <Callum.yates.180@gmail.com>
2025-04-07 16:57:47 +0100

    Update 'description' column in 'products' table migration to allow null values

--- database/migrations/2025_02_06_165422_create_products_table.php
+++ database/migrations/2025_02_06_165422_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->decimal('price', 8, 2);
             $table->boolean('available');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('category');
             $table->date('sell_by_date');
             $table->timestamps();
